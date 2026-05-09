140a40f64dde87e90dbbb0f142d712b8e9d821bd
JRSiwiecki <JRSiwiecki@gmail.com>
2026-05-09 01:10:19 -0700

    what is wrong with these guys

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import { AppShell, Burger } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import Navbar from "./components/Navbar";
-
-import "./App.css";
 import { Outlet } from "react-router-dom";
+import "./App.css";
 import ToggleColorScheme from "./components/ToggleColorScheme";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   const [opened, { toggle }] = useDisclosure();
 
   return (
     <AppShell
-      padding="md"
+      padding="sm"
       header={{ height: 60 }}
       navbar={{
-        width: 300,
+        width: 200,
         breakpoint: "sm",
-        collapsed: { mobile: !opened },
+        collapsed: { desktop: !opened, mobile: !opened },
       }}
     >
       <AppShell.Header>
-        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+        <Burger opened={opened} onClick={toggle} />
         <ToggleColorScheme />
       </AppShell.Header>
 
