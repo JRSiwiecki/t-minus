d2fa479583c00794ce831a6bdeb9dd7326623550
JRSiwiecki <JRSiwiecki@gmail.com>
2026-05-09 02:58:09 -0700

    prefetch?

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,6 +1,9 @@
 import { Title } from "@mantine/core";
+import { usePrefetchLaunchList } from "../launch-list/hooks/useGetLaunchList";
 
 export default function Home() {
+  usePrefetchLaunchList();
+
   return (
     <>
       <Title order={2}>Home</Title>
--- src/features/launch-list/hooks/useGetLaunchList.ts
+++ src/features/launch-list/hooks/useGetLaunchList.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { usePrefetchQuery, useQuery } from "@tanstack/react-query";
 import { QUERY_KEY_DICTIONARY } from "../../../constants/query-key-dictionary";
 import { getLaunchList } from "../../../services/api";
 import { minutesToMilliseconds } from "date-fns";
@@ -13,4 +13,11 @@ const useGetLaunchListQuery = () => {
 
 export const useGetLaunchList = () => {
   return useGetLaunchListQuery();
+};
+
+export const usePrefetchLaunchList = () => {
+  usePrefetchQuery({
+    queryKey: QUERY_KEY_DICTIONARY.GET_LAUNCH_LIST,
+    queryFn: getLaunchList,
+  });
 };
