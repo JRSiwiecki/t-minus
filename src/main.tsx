66912e0f6e589139ceb19f07e8edf3ba32ef5e55
JRSiwiecki <JRSiwiecki@gmail.com>
2026-05-09 00:06:27 -0700

    mantine and postcss

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
 import "./index.css";
 import App from "./App.tsx";
+
+import "@mantine/core/styles.css";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <MantineProvider>
+        <App />
+      </MantineProvider>
     </QueryClientProvider>
   </StrictMode>,
 );
