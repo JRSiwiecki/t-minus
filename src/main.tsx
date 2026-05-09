0d5718f439fb07a67fbe2737f399296f7758e5e9
JRSiwiecki <JRSiwiecki@gmail.com>
2026-05-09 00:17:57 -0700

    add react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,22 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MantineProvider } from "@mantine/core";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App.tsx";
-
 import "@mantine/core/styles.css";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <MantineProvider>
-        <App />
-      </MantineProvider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <MantineProvider>
+          <App />
+        </MantineProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 );
