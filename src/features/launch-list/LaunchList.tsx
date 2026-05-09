3aecdeac19560799d2a3a1dbb370722801293c51
JRSiwiecki <JRSiwiecki@gmail.com>
2026-05-09 01:34:39 -0700

    fix breakpoint

--- src/features/launch-list/LaunchList.tsx
+++ src/features/launch-list/LaunchList.tsx
@@ -1,9 +1,10 @@
-import { Title } from "@mantine/core";
+import { Skeleton, Stack, Title } from "@mantine/core";
 
 export default function LaunchList() {
   return (
-    <>
+    <Stack align="center">
       <Title order={2}>Launch List</Title>
-    </>
+      <Skeleton height={1000} w="97%" m="sm" />
+    </Stack>
   );
 }
