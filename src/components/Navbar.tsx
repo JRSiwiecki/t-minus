eaf95ec4e785dd22696a6dd1962858b8771628d2
JRSiwiecki <JRSiwiecki@gmail.com>
2026-05-09 01:19:01 -0700

    navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,26 @@
 import { NavLink } from "@mantine/core";
 import { HouseSimpleIcon, RocketLaunchIcon } from "@phosphor-icons/react";
+import { useLocation } from "react-router-dom";
+
+const ICON_SIZE = 20;
 
 export default function Navbar() {
+  const location = useLocation();
+  const path = location.pathname;
+
   return (
     <>
-      <NavLink href="/" label="Home" leftSection={<HouseSimpleIcon size={20} />} />
+      <NavLink
+        href="/"
+        label="Home"
+        leftSection={<HouseSimpleIcon size={ICON_SIZE} />}
+        active={path === "/"}
+      />
       <NavLink
         href="/launch-list"
         label="Launch List"
-        leftSection={<RocketLaunchIcon size={20} />}
+        leftSection={<RocketLaunchIcon size={ICON_SIZE} />}
+        active={path === "/launch-list"}
       />
     </>
   );
