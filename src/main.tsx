d926425460be8090920fd296d6cd961473f8f70f
JRSiwiecki <JRSiwiecki@gmail.com>
2026-05-09 01:53:09 -0700

    react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -9,13 +9,24 @@ import App from "./App.tsx";
 import "@mantine/core/styles.css";
 import LaunchList from "./features/launch-list/LaunchList.tsx";
 import Home from "./features/home/Home.tsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
+
+// This code is only for TypeScript
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: import("@tanstack/query-core").QueryClient;
+  }
+}
+
+window.__TANSTACK_QUERY_CLIENT__ = queryClient;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <MantineProvider defaultColorScheme="dark">
           <Routes>
             <Route path="/" element={<App />}>
